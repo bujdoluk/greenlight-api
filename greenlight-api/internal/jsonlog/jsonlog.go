eed4ba5bd18f7805fe9439cba4ef7fcfc0635a68
Lukáš Bujdoš <lukas.bujdos@gmail.com>
2025-02-07 20:25:10 +0100

    feat: added metadata struct and custom json logger

---
+++ greenlight-api/internal/jsonlog/jsonlog.go
@@ -0,0 +1,41 @@
+package jsonlog
+
+import (
+	"io"
+	"sync"
+)
+
+type Level int8
+
+const (
+	LevelInfo Level = iota
+	LevelError
+	LevelFatal
+	LevelOff
+)
+
+func (l Level) String() string {
+	switch l {
+	case LevelInfo:
+		return "INFO"
+	case LevelError:
+		return "ERROR"
+	case LevelFatal:
+		return "FATAL"
+	default:
+		return ""
+	}
+}
+
+type Logger struct {
+	out      io.Writer
+	minLevel Level
+	mu       sync.Mutex
+}
+
+func New(out io.Writer, minLevel Level) *Logger {
+	return &Logger{
+		out:      out,
+		minLevel: minLevel,
+	}
+}
