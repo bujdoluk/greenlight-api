469ce68061e235201cff861baee5aa3771fa8422
Lukáš Bujdoš <lukas.bujdos@gmail.com>
2025-02-09 18:51:16 +0100

    feat: added build command for creating linux binaries

--- greenlight-api/cmd/api/middleware.go
+++ greenlight-api/cmd/api/middleware.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"net"
 	"net/http"
 	"sync"
 	"time"
 
+	"github.com/tomasen/realip"
 	"golang.org/x/time/rate"
 )
 
@@ -53,11 +53,7 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if app.config.limiter.enabled {
-			ip, _, err := net.SplitHostPort(r.RemoteAddr)
-			if err != nil {
-				app.serverErrorResponse(w, r, err)
-				return
-			}
+			ip := realip.FromRequest(r)
 
 			mu.Lock()
 
